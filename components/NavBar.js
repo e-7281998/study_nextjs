6deb520cf6c374e24e7010c7be4a7e5925cc3706
e-7281998 <jejjcs43@naver.com>
2023-01-19 17:28:49 +0900

    style, _app 커스터마이징

---
+++ components/NavBar.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavBar() {
+    const router = useRouter();
+
+    return (
+        <nav>
+            <Link legacyBehavior
+                href="/">
+                <a className={router.pathname === "/" ? "active" : ''}>Home</a>
+            </Link>
+            <Link legacyBehavior
+                href="/about">
+                <a className={router.pathname === "/about" ? "active" : ''}>About</a>
+            </Link>
+            <style jsx>{`
+            nav {
+                background-color: lightgrey;
+            }
+            a {
+                text-decoration: none;
+            }
+            .active {
+                color: red;
+            }
+            `}</style>
+        </nav >
+    )
+}
