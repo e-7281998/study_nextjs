67199864b749b991ded15eace490e2752507d18d
e-7281998 <jejjcs43@naver.com>
2023-02-26 12:27:21 +0900

    SSR : loading 없애기 : getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,13 @@
 import Seo from "@/components/Seo";
 import { useEffect, useState } from "react";
 
-export default function Home() {
-    const [movies, setMovies] = useState();
-    useEffect(() => {
-        (async () => {
-            const { results } = await (
-                await fetch(
-                    `/api/movies`
-                )
-            ).json();
-            setMovies(results);
-        })();
-    }, []);
+export default function Home({ results }) {
 
     return (
         <div className="container">
             <Seo title="Home" />
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map(movie => (
+            {!results && <h4>Loading...</h4>}
+            {results?.map(movie => (
                 <div className="movie" key={movie.id}>
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                     <h4>{movie.original_title}</h4>
@@ -55,4 +44,27 @@ export default function Home() {
         </div>
 
     );
+}
+
+//무조건 서버에서 돌아감
+export async function getServerSideProps() {
+    const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+
+    return {
+        props: {
+            results,
+        },
+    }
+
+    // useEffect(() => {
+    //     (async () => {
+    //         const { results } = await (
+    //             await fetch(
+    //                 `/api/movies`
+    //             )
+    //         ).json();
+    //         setMovies(results);
+    //     })();
+    // }, []);
+
 }
